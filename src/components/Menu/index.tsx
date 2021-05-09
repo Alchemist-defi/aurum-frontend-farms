194c0c78c3f9812f3b6aeb1a88ef274142dc7e29
ggoosen <georgegoosen@gmail.com>
2021-05-09 17:20:04 +0930

    Pricing update
    Updated Aurum Frontend to show mist and aurum prices on the menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,7 +3,7 @@ import { useWallet } from '@binance-chain/bsc-use-wallet'
 import { allLanguages } from 'config/localisation/languageCodes'
 import { LanguageContext } from 'contexts/Localisation/languageContext'
 import useTheme from 'hooks/useTheme'
-import { usePriceCakeBusd } from 'state/hooks'
+import { usePriceCakeBusd, usePriceMistBusd } from 'state/hooks'
 import { Menu as UikitMenu } from '@pancakeswap-libs/uikit'
 import config from './config'
 
@@ -11,7 +11,9 @@ const Menu = (props) => {
   const { account, connect, reset } = useWallet()
   const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
   const { isDark, toggleTheme } = useTheme()
-  const cakePriceUsd = usePriceCakeBusd()
+  // const cakePriceUsd = usePriceCakeBusd()
+  const mistPriceUsd = usePriceMistBusd()
+  const aurumPriceUsd = usePriceCakeBusd()  
 
   return (
     <UikitMenu
@@ -23,9 +25,11 @@ const Menu = (props) => {
       currentLang={selectedLanguage && selectedLanguage.code}
       langs={allLanguages}
       setLang={setSelectedLanguage}
-      cakePriceUsd={cakePriceUsd.toNumber()}
+      aurumPriceUsd={aurumPriceUsd.toNumber()}
+      mistPriceUsd={mistPriceUsd.toNumber()}
       links={config}
-      priceLink="https://exchange.alchemistdefi.com"
+      mistPriceLink="https://charts.bogged.finance/?token=0x6f8FE12CC34398d15b7D5A5BA933E550DA1D099f"
+      aurumPriceLink="https://charts.bogged.finance/?token=0x49207BAA3a7332F0716788aa57B088D499bcc104"
       {...props}
     />
   )
